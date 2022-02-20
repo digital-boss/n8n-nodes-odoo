84213cd6d79688210ae23597a75d2417d71d44aa
Valentina <valentina.lilova98@gmail.com>
2022-02-20 19:47:49 +0200

    More commits at https://github.com/n8n-io/n8n/pull/2601

--- nodes/Odoo/GenericFunctions.ts
+++ nodes/Odoo/GenericFunctions.ts
@@ -221,8 +221,9 @@ export async function odooGet(
 					password,
 					mapOdooResources[resource] || resource,
 					mapOperationToJSONRPC[operation],
-					[itemsID] || [],
-					(fieldsToReturn && processResponceFields(fieldsToReturn)) || [],
+					[+itemsID] || [],
+					// (fieldsToReturn && processResponceFields(fieldsToReturn)) || [],
+					fieldsToReturn || [],
 				],
 			},
 			id: Math.floor(Math.random() * 100),
@@ -245,7 +246,6 @@ export async function odooGetAll(
 	url: string,
 	filters?: IOdooFilterOperations,
 	fieldsToReturn?: IDataObject,
-	offset = 0,
 	limit = 0,
 ) {
 	try {
@@ -262,8 +262,9 @@ export async function odooGetAll(
 					mapOdooResources[resource] || resource,
 					mapOperationToJSONRPC[operation],
 					(filters && processFilters(filters)) || [],
-					(fieldsToReturn && processResponceFields(fieldsToReturn)) || [],
-					offset,
+					// (fieldsToReturn && processResponceFields(fieldsToReturn)) || [],
+					fieldsToReturn || [],
+					0, // offset
 					limit,
 				],
 			},
@@ -301,7 +302,7 @@ export async function odooUpdate(
 					password,
 					mapOdooResources[resource] || resource,
 					mapOperationToJSONRPC[operation],
-					[itemsID] || [],
+					[+itemsID] || [],
 					fieldsToUpdate,
 				],
 			},
@@ -309,7 +310,7 @@ export async function odooUpdate(
 		};
 
 		const result = await odooJSONRPCRequest.call(this, body, url);
-		return { success: result };
+		return { success: true };
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error as JsonObject);
 	}
@@ -338,14 +339,14 @@ export async function odooDelete(
 					password,
 					mapOdooResources[resource] || resource,
 					mapOperationToJSONRPC[operation],
-					[itemsID] || [],
+					[+itemsID] || [],
 				],
 			},
 			id: Math.floor(Math.random() * 100),
 		};
 
 		const result = await odooJSONRPCRequest.call(this, body, url);
-		return { success: result };
+		return { success: true };
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error as JsonObject);
 	}
